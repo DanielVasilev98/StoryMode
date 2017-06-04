1bdaaf1fb80ee8c2b6aad489d80659194b908f99
DanielVasilev98 <kyn_aguero_10@abv.bg>
2017-06-04 12:44:05 +0300

    Add project files.

---
+++ BashSoft/IOManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+   public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            OutputWriter.WriteMessageOnLine(path);
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+                identation++;
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    
+                    OutputWriter.WriteMessageOnLine(string.Format("{0}{1}", new string('-', identation), directoryPath));
+                    subFolders.Enqueue(directoryPath);
+                }
+                
+            }
+        }
+    }
+}
---
+++ BashSoft/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOManager.TraverseDirectory(@"C:\Users\D & B\Documents\visual studio 2017\Projects\StoryMode");
+        }
+    }
+}
